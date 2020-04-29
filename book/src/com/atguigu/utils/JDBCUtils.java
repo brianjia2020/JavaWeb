f9e7947fe0b753c0b837c212043653be925bd42c
brian.jia <brian.jia@snowflake.com>
2020-04-28 20:50:19 -0400

    book project

---
+++ book/src/com/atguigu/utils/JDBCUtils.java
@@ -0,0 +1,20 @@
+package com.atguigu.utils;
+
+import java.sql.Connection;
+
+public class JDBCUtils {
+    /*
+     * to get connection
+     */
+    public static Connection getConnection(){
+        return null;
+    }
+
+    /*
+     * close connection
+     */
+
+    public static void closeConnection(Connection conn){
+
+    }
+}
